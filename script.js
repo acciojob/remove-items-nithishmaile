25646501aa123625e1e147345b8a59b2e1481025
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-10 09:12:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const selectElement=document.getElementById("colorSelect");
+const removeButton=document.getElementById("removeButton");
+
+removeButton.addEventListener("click",()=>{
+	const selectedOption=selectElement.options[selectElement.selectedIndex]
+	if(selectedOption){
+		selectedOption.remove()
+	}else{
+		console.log("NO OPTION IS SELECTED");
+	}
+})
